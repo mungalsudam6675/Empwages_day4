f4ba293593d923765c4205d583f60cc0ac254140
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-01-25 11:35:28 +0530

    Adding usecase 1

---
+++ Uc1.java
@@ -0,0 +1,9 @@
+public class Uc1{
+        public static void main (String [] a){
+                double empCheck = Math.floor ( Math.random() * 2 );
+                if(empCheck == 1)
+                        System.out.println("Employee is Present");
+                else
+                        System.out.println("Employee is Absent");
+}
+}
